e045e9de371c14b336640f856a97ae883fabbd2e
“Sahithya <sahithya.hanumandla01@gmail.com>
2024-04-15 19:13:32 -0400

    useState example project done

--- birthday-buddy/src/App.jsx
+++ birthday-buddy/src/App.jsx
@@ -1,4 +1,38 @@
+import { useState } from 'react'
+import data from './data'
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
-export default App;
+  const [people, setPeople] = useState(data)
+  const no_of_people = people.length
+
+  const handleClick = () => {
+    setPeople([])
+  }
+
+  return (
+    <div className="container">
+      <h3>{no_of_people} birthdays today</h3>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id} className="person">
+            <img
+              src={person.image}
+              alt={person.name}
+              className="img"
+              style={{ height: '100px', width: '100px' }}
+            />
+            <div>
+              <h2>{person.name}</h2>
+              <h5>Age: {person.age}</h5>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button className="btn btn-block" type="button" onClick={handleClick}>
+        Clear all
+      </button>
+    </div>
+  )
+}
+
+export default App
